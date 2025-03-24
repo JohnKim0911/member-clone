3e89afb38015085c65ddfe3b544b56d5faabaeec
John Kim <john.donggyu.kim@gmail.com>
2025-03-24 18:17:50 +0900

    [06_로그인]완료

--- src/main/java/com/clone/member/controller/MemberController.java
+++ src/main/java/com/clone/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.clone.member.controller;
 
 import com.clone.member.dto.MemberDTO;
 import com.clone.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,24 @@ public class MemberController {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
-        return "index";
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
     }
 }
